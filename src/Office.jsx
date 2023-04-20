2373778685700315311c7871b0c613372aa3eaad
hungikim <gns213@gmail.com>
2023-04-19 23:44:20 -0400

    fixed location image path

--- src/Office.jsx
+++ src/Office.jsx
@@ -1,4 +1,5 @@
 import './Office.css'
+import greenLocation from './assets/green-location.png'
 
 export default function Office(){
     return (
@@ -10,7 +11,7 @@ export default function Office(){
                 <p>FAX: 031-439-3466</p>
             </div>
             <div className='map-container'>
-                <img className='map-image' onClick={ () => window.open('https://naver.me/Gd1uu2DP','_blank') } src='./src/assets/green-location.png' alt='그린세무법인 위치' width='800px' height='400px' />
+            <img className='map-image' onClick={ () => window.open('https://naver.me/Gd1uu2DP','_blank') } src={greenLocation} alt='그린세무법인 위치' width='800px' height='400px' />
                 <p className='map-comment'>지도를 클릭하시면 새 탭에서 지도가 열립니다.</p>
             </div>
         </div>
