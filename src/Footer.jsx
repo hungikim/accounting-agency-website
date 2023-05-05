152ce261bf60671ff6a0da99415a22abfff00fec
hungikim <gns213@gmail.com>
2023-05-05 13:40:15 -0400

    polished footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,8 +8,8 @@ export default function Footer(){
                 <p><a href='tel:0314393455'>031-439-3455</a> (월~금 09:00 ~ 17:00)</p>
             </div>
             <div className='footer-info'>
-                <p className='footer-info-title'>위치</p>
-                <p>ㄴㅇㄹㄴㅇㄹㅇㄴㄹ</p>
+                <p className='footer-info-title'>그린세무법인</p>
+                <p>경기도 안산시 광덕4로 116 304호</p>
             </div>
         </footer>
     )
