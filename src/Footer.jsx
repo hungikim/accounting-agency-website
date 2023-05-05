5383cf40de824dd25e32f28acb8b58b6a9460704
hungikim <gns213@gmail.com>
2023-05-05 13:26:58 -0400

    changed font

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,35 +3,13 @@ import './Footer.css'
 export default function Footer(){
     return (
         <footer className='Footer'>
-            <div className='footer-container'>
-                <div className='footer-tel-info'>
-                    <p className='footer-column-title'>전화상담</p>
-                    <p><a href='tel:0314393455'>031-439-3455</a> (월~금 09:00 ~ 17:00)</p>
-                </div>
-                <div className='footer-break'></div>
-                <ul className="footer-column">
-                    <li className='footer-column-title'>제목1</li>
-                    <li>항목1</li>
-                    <li>항목2</li>
-                    <li>항목3</li>
-                </ul>
-                <ul className="footer-column">
-                    <li className='footer-column-title'>제목2</li>
-                    <li>항목1</li>
-                    <li>항목2</li>
-                    <li>항목3</li>
-                </ul>
-                <ul className="footer-column">
-                    <li className='footer-column-title'>제목3</li>
-                    <li>항목1</li>
-                    <li>항목2</li>
-                </ul>
-                <ul className="footer-column">
-                    <li className='footer-column-title'>제목4</li>
-                    <li>항목1</li>
-                    <li>항목2</li>
-                    <li>항목3</li>
-                </ul>
+            <div className='footer-info'>
+                <p className='footer-info-title'>전화상담</p>
+                <p><a href='tel:0314393455'>031-439-3455</a> (월~금 09:00 ~ 17:00)</p>
+            </div>
+            <div className='footer-info'>
+                <p className='footer-info-title'>위치</p>
+                <p>ㄴㅇㄹㄴㅇㄹㅇㄴㄹ</p>
             </div>
         </footer>
     )
